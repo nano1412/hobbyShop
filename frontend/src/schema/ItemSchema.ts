d9c6fd945ab7e97f50baee276d7d8223e4c5ced5
nano1412_15irx11 <nano.khongsri@gmail.com>
2026-04-09 16:21:03 +0700

    feat add/edit schema

--- frontend/src/schema/ItemSchema.ts
+++ frontend/src/schema/ItemSchema.ts
@@ -1,85 +1,48 @@
 import z from 'zod/v4'
+import {
+  ColorTone,
+  GunplaExclusivity,
+  GunplaGrade,
+  LiquidProductType,
+  MsrpCurrency,
+  PaintApplicationMethod,
+  PaintFinish,
+  PaintSpecialPorperty,
+  ResinType,
+} from './interface'
 
-export const addItemSchema = z.object({
+export const itemSchema = z.object({
   userId: z.string().optional(),
   id: z.int().optional(), //for update
   categoryId: z.union([z.string(), z.int()]), // integer
   name: z.string(),
   description: z.string(),
-  thumbnailPath: z.string(),
+
+  thumbnailPath: z.string().optional(),
+  thumbnailId: z.string().optional(),
   imageFile: z.file().optional(),
+
   brand: z.string(),
   stockQty: z.int(), // integer
   storePriceThb: z.number(), // float
   msrpPrice: z.number(), // float
-  msrpCurrency: z.enum(['THB', 'JPY', 'CNY', 'USD']), // replace with your Prisma enum values
+  msrpCurrency: z.enum(MsrpCurrency).optional(), // replace with your Prisma enum values
   releaseYear: z.int(),
 
-  gunplaGrade: z
-    .enum([
-      'other',
-      'NG',
-      'SD',
-      'EG',
-      'HG',
-      'RG',
-      'MG',
-      'PG',
-      'RE100',
-      'FM',
-      'MGSD',
-      'MEGA',
-    ])
-    .optional(), // Prisma enum mapping
-  gunplaExclusivity: z
-    .enum([
-      'none',
-      'gundam_base_limited',
-      'p_bandai',
-      'event',
-      'special_package',
-    ])
-    .optional(), // Prisma enum mapping
+  gunplaGrade: z.enum(GunplaGrade).optional(), // Prisma enum mapping
+  gunplaExclusivity: z.enum(GunplaExclusivity).optional(), // Prisma enum mapping
 
   fromSerie: z.string().optional(),
   height: z.number().optional(), // float
 
-  liquidProductType: z
-    .enum(['other', 'paint', 'primer', 'solvent', 'thinner', 'cement'])
-    .optional(),
-  resinType: z
-    .enum(['none', 'acrylic', 'lacquer', 'enamel', 'epoxy'])
-    .optional(),
+  liquidProductType: z.enum(LiquidProductType).optional(),
+  resinType: z.enum(ResinType).optional(),
   volumeMl: z.number().optional(),
 
-  colorTone: z
-    .enum([
-      'other',
-      'red',
-      'orange',
-      'yellow',
-      'green',
-      'cyan',
-      'blue',
-      'purple',
-      'black',
-      'white',
-      'gray',
-      'pink',
-      'brown',
-
-      'gold',
-      'silver',
-      'copper',
-    ])
-    .optional(),
-  paintSpecialPorperty: z.enum(['none', 'clear', 'metalic']).optional(),
-  paintApplicationMethod: z
-    .enum(['other', 'spray', 'brush', 'air_brush_ready', 'panel_liner'])
-    .optional(),
-  paintFinish: z
-    .enum(['other', 'gloss', 'semi_gloss', 'satin', 'matte'])
-    .optional(),
+  colorTone: z.enum(ColorTone).optional(),
+  paintSpecialPorperty: z.enum(PaintSpecialPorperty).optional(),
+  paintApplicationMethod: z.enum(PaintApplicationMethod).optional(),
+  paintFinish: z.enum(PaintFinish).optional(),
 })
 
-export type addItem = z.infer<typeof addItemSchema>
+export type item = z.infer<typeof itemSchema>
