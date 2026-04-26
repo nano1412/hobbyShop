4cdbbcf87f856a8e6c5ae43a8118e8759e1492e6
nano1412_15irx11 <nano.khongsri@gmail.com>
2026-04-26 07:00:28 +0700

    fix add loading screen and remove temp data

--- frontend/src/components/pages/view-item/ViewItem.tsx
+++ frontend/src/components/pages/view-item/ViewItem.tsx
@@ -17,34 +17,22 @@ export default function ViewItemUI() {
   const { id } = useParams({ from: '/view-item/$id' })
   const [loading, setLoading] = useState(false)
   const [resultItem, setResultItem] = useState<itemResponse | null>({
-    id: 7,
-    categoryId: 7,
-    name: 'A',
-    description:
-      'eqeqzxczxcczzxczxzxczxzxczxweeqeqzxczxcczzxczxzxczxzxczxweeqeqzxczxcczzxczxzxczxzxczxwe',
-    thumbnailPath: 'https://ik.imagekit.io/nano1412/dogimages_NCpqnrCXB.jpg',
-    thumbnailId: '69d895bc5c7cd75eb8f11f56',
-    brand: 'zxc',
-    stockQty: 22222,
-    storePriceThb: 1113,
-    msrpPrice: 22,
-    msrpCurrency: 'THB',
-    releaseYear: 22,
-    createdAt: '2026-04-21T09:29:47.910Z',
-    createdBy: 'JSy8QAuMSxIZJwIhfS7YzS5mMr7NmDYd',
-    updatedAt: '2026-04-21T09:29:47.910Z',
-    updatedBy: 'JSy8QAuMSxIZJwIhfS7YzS5mMr7NmDYd',
-    gunplaGrade: 'PG',
-    gunplaExclusivity: 'event',
-    fromSerie: 'UC',
-    height: 12,
-    liquidProductType: 'solvent',
-    resinType: 'lacquer',
-    volumeMl: 123123,
-    colorTone: 'cyan',
-    paintSpecialPorperty: 'none',
-    paintApplicationMethod: 'spray',
-    paintFinish: 'matte',
+    id: 0,
+    categoryId: 0,
+    name: '',
+    description: '',
+    thumbnailPath: '',
+    thumbnailId: '',
+    brand: '',
+    stockQty: 0,
+    storePriceThb: 0,
+    msrpPrice: 0,
+    msrpCurrency: '',
+    releaseYear: 0,
+    createdAt: '',
+    createdBy: '',
+    updatedAt: '',
+    updatedBy: '',
   })
   const [error, setError] = useState<string | null>(null)
   const [createdItemUser, setCreatedItemUser] = useState<string>('')
@@ -97,184 +85,197 @@ export default function ViewItemUI() {
   }
 
   useEffect(() => {
-    // callItem()
+    callItem()
     getUserName()
   }, [])
 
   return (
     <>
       <Header />
-      <div className=" mx-20 p-5 ">
-        <h1 className="text-3xl">View item: {resultItem.name}</h1>
-        <div className="bg-gray-50 rounded-2xl drop-shadow-xl p-5 mt-5">
-          <div className="grid grid-cols-3 gap-6">
-            <div className="col-span-2 space-y-4">
-              <DataWithLabel label="Item Name" text={resultItem.name} />
-              <div className="grid grid-cols-2 gap-4">
-                <DataWithLabel label="Category" text="">
-                  <CategoryPill categoryId={resultItem.categoryId} />
-                </DataWithLabel>
-                <DataWithLabel
-                  label="Release Year"
-                  text={resultItem.releaseYear}
-                />
-              </div>
-              <DataWithLabel label="Brand" text={resultItem.brand} />
-              <div className="grid grid-cols-2 gap-4">
-                <DataWithLabel
-                  label="Stock Quantity"
-                  text={resultItem.stockQty}
-                />
-                <DataWithLabel
-                  label="Store Price (THB)"
-                  text={resultItem.storePriceThb}
-                />
-              </div>
+      {loading ? (
+        <div className="flex flex-col items-center justify-center h-64 gap-3">
+          <div className="w-8 h-8 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"></div>
+          <p className="text-gray-600">getting item data...</p>
+        </div>
+      ) : (
+        <div className=" mx-20 p-5 ">
+          <h1 className="text-3xl">View item: {resultItem.name}</h1>
+          <div className="bg-gray-50 rounded-2xl drop-shadow-xl p-5 mt-5">
+            <div className="grid grid-cols-3 gap-6">
+              <div className="col-span-2 space-y-4">
+                <DataWithLabel label="Item Name" text={resultItem.name} />
+                <div className="grid grid-cols-2 gap-4">
+                  <DataWithLabel label="Category" text="">
+                    <CategoryPill categoryId={resultItem.categoryId} />
+                  </DataWithLabel>
+                  <DataWithLabel
+                    label="Release Year"
+                    text={resultItem.releaseYear}
+                  />
+                </div>
+                <DataWithLabel label="Brand" text={resultItem.brand} />
+                <div className="grid grid-cols-2 gap-4">
+                  <DataWithLabel
+                    label="Stock Quantity"
+                    text={resultItem.stockQty}
+                  />
+                  <DataWithLabel
+                    label="Store Price (THB)"
+                    text={resultItem.storePriceThb}
+                  />
+                </div>
 
-              <div className="grid grid-cols-2 gap-4">
-                <DataWithLabel label="MSRP" text={resultItem.msrpPrice} />
+                <div className="grid grid-cols-2 gap-4">
+                  <DataWithLabel label="MSRP" text={resultItem.msrpPrice} />
+                  <DataWithLabel
+                    label="MSRP Currency"
+                    text={resultItem.msrpCurrency}
+                  />
+                </div>
+              </div>
+              <div>
+                {/* image */}
+                <div className=" min-w-full min-h-3xs grid place-items-center overflow-hidden">
+                  <Center className="w-70 h-70 rounded-2xl border-2 border-dashed border-gray-400">
+                    {resultItem.thumbnailPath ? (
+                      <Image
+                        src={resultItem.thumbnailPath}
+                        alt="thumbnail"
+                        w={250}
+                        h={250}
+                        radius="sm"
+                        fit="cover"
+                      />
+                    ) : (
+                      <IconPhotoOff size={80} stroke={1.5} />
+                    )}
+                  </Center>
+                </div>
                 <DataWithLabel
-                  label="MSRP Currency"
-                  text={resultItem.msrpCurrency}
+                  label="Description"
+                  text={resultItem.description}
                 />
               </div>
             </div>
-            <div>
-              {/* image */}
-              <div className=" min-w-full min-h-3xs grid place-items-center overflow-hidden">
-                <Center className="w-70 h-70 rounded-2xl border-2 border-dashed border-gray-400">
-                  {resultItem.thumbnailPath ? (
-                    <Image
-                      src={resultItem.thumbnailPath}
-                      alt="thumbnail"
-                      w={250}
-                      h={250}
-                      radius="sm"
-                      fit="cover"
+          </div>
+
+          {haveAdditionalDetail() && (
+            <div className="bg-gray-100 p-5 mt-5 rounded-2xl drop-shadow-xl">
+              <h2 className="text-3xl mt-5">Additional Detail</h2>
+
+              {(!!resultItem.fromSerie || !!resultItem.height) && (
+                <div className="bg-white rounded-2xl drop-shadow-xl p-5 mt-5">
+                  <h2> Figure Common</h2>
+                  <div className="grid grid-cols-2 gap-4">
+                    <DataWithLabel
+                      label="From Serie"
+                      text={resultItem.fromSerie}
                     />
-                  ) : (
-                    <IconPhotoOff size={80} stroke={1.5} />
-                  )}
-                </Center>
-              </div>
-              <DataWithLabel
-                label="Description"
-                text={resultItem.description}
-              />
-            </div>
-          </div>
-        </div>
+                    <DataWithLabel
+                      label="Height (CM)"
+                      text={resultItem.height}
+                    />
+                  </div>
+                </div>
+              )}
 
-        {haveAdditionalDetail() && (
+              {(!!resultItem.gunplaGrade || !!resultItem.gunplaExclusivity) && (
+                <div className="bg-white rounded-2xl drop-shadow-xl p-5 mt-5">
+                  <h2> Gunpla Detail</h2>
+                  <div className="grid grid-cols-2 gap-4">
+                    <DataWithLabel
+                      label="Gunpla Grade"
+                      text={resultItem.gunplaGrade}
+                    />
+                    <DataWithLabel
+                      label="Exclusivity"
+                      text={resultItem.gunplaExclusivity}
+                    />
+                  </div>
+                </div>
+              )}
+
+              {(!!resultItem.liquidProductType ||
+                !!resultItem.resinType ||
+                !!resultItem.volumeMl) && (
+                <div className="bg-white rounded-2xl drop-shadow-xl p-5 mt-5">
+                  <h2> Liquid Product</h2>
+                  <div className="grid grid-cols-3 gap-4">
+                    <DataWithLabel
+                      label="Liquid Product Type"
+                      text={resultItem.liquidProductType}
+                    />
+                    <DataWithLabel
+                      label="Resin Type"
+                      text={resultItem.resinType}
+                    />
+                    <DataWithLabel
+                      label="Volume (ML)"
+                      text={resultItem.volumeMl}
+                    />
+                  </div>
+                </div>
+              )}
+
+              {(!!resultItem.colorTone ||
+                !!resultItem.paintSpecialPorperty ||
+                !!resultItem.paintApplicationMethod ||
+                !!resultItem.paintFinish) && (
+                <div className="bg-white rounded-2xl drop-shadow-xl p-5 mt-5">
+                  <h2> Figure Common</h2>
+                  <div className="grid grid-cols-2 gap-4">
+                    <DataWithLabel
+                      label="Color Tone"
+                      text={resultItem.colorTone}
+                    />
+                    <DataWithLabel
+                      label="Paint Special Porperty"
+                      text={resultItem.paintSpecialPorperty}
+                    />
+                    <DataWithLabel
+                      label="Paint Application Method"
+                      text={resultItem.paintApplicationMethod}
+                    />
+                    <DataWithLabel
+                      label="Paint Finish"
+                      text={resultItem.paintFinish}
+                    />
+                  </div>
+                </div>
+              )}
+            </div>
+          )}
           <div className="bg-gray-100 p-5 mt-5 rounded-2xl drop-shadow-xl">
-            <h2 className="text-3xl mt-5">Additional Detail</h2>
-
-            {(!!resultItem.fromSerie || !!resultItem.height) && (
-              <div className="bg-white rounded-2xl drop-shadow-xl p-5 mt-5">
-                <h2> Figure Common</h2>
-                <div className="grid grid-cols-2 gap-4">
-                  <DataWithLabel
-                    label="From Serie"
-                    text={resultItem.fromSerie}
-                  />
-                  <DataWithLabel label="Height (CM)" text={resultItem.height} />
-                </div>
+            <h2 className="text-3xl mt-5">System Infomation</h2>
+            <div className="bg-white rounded-2xl drop-shadow-xl p-5 mt-5">
+              <div className="grid grid-cols-2 gap-4">
+                <DataWithLabel label="Item id" text={resultItem.id} />
+                <div />
+                <DataWithLabel
+                  label="Created At"
+                  text={resultItem.createdAt.toString()}
+                />
+                <DataWithLabel label="Created By" text={createdItemUser} />
+                <DataWithLabel
+                  label="Last Updated At"
+                  text={resultItem.updatedAt.toString()}
+                />
+                <DataWithLabel label="Last Updated By" text={updatedItemUser} />
               </div>
-            )}
-
-            {(!!resultItem.gunplaGrade || !!resultItem.gunplaExclusivity) && (
-              <div className="bg-white rounded-2xl drop-shadow-xl p-5 mt-5">
-                <h2> Gunpla Detail</h2>
-                <div className="grid grid-cols-2 gap-4">
-                  <DataWithLabel
-                    label="Gunpla Grade"
-                    text={resultItem.gunplaGrade}
-                  />
-                  <DataWithLabel
-                    label="Exclusivity"
-                    text={resultItem.gunplaExclusivity}
-                  />
-                </div>
-              </div>
-            )}
-
-            {(!!resultItem.liquidProductType ||
-              !!resultItem.resinType ||
-              !!resultItem.volumeMl) && (
-              <div className="bg-white rounded-2xl drop-shadow-xl p-5 mt-5">
-                <h2> Liquid Product</h2>
-                <div className="grid grid-cols-3 gap-4">
-                  <DataWithLabel
-                    label="Liquid Product Type"
-                    text={resultItem.liquidProductType}
-                  />
-                  <DataWithLabel
-                    label="Resin Type"
-                    text={resultItem.resinType}
-                  />
-                  <DataWithLabel
-                    label="Volume (ML)"
-                    text={resultItem.volumeMl}
-                  />
-                </div>
-              </div>
-            )}
-
-            {(!!resultItem.colorTone ||
-              !!resultItem.paintSpecialPorperty ||
-              !!resultItem.paintApplicationMethod ||
-              !!resultItem.paintFinish) && (
-              <div className="bg-white rounded-2xl drop-shadow-xl p-5 mt-5">
-                <h2> Figure Common</h2>
-                <div className="grid grid-cols-2 gap-4">
-                  <DataWithLabel
-                    label="Color Tone"
-                    text={resultItem.colorTone}
-                  />
-                  <DataWithLabel
-                    label="Paint Special Porperty"
-                    text={resultItem.paintSpecialPorperty}
-                  />
-                  <DataWithLabel
-                    label="Paint Application Method"
-                    text={resultItem.paintApplicationMethod}
-                  />
-                  <DataWithLabel
-                    label="Paint Finish"
-                    text={resultItem.paintFinish}
-                  />
-                </div>
-              </div>
-            )}
-          </div>
-        )}
-        <div className="bg-gray-100 p-5 mt-5 rounded-2xl drop-shadow-xl">
-          <h2 className="text-3xl mt-5">System Infomation</h2>
-          <div className="bg-white rounded-2xl drop-shadow-xl p-5 mt-5">
-            <div className="grid grid-cols-2 gap-4">
-              <DataWithLabel label="Item id" text={resultItem.id} />
-              <div />
-              <DataWithLabel label="Created At" text={resultItem.createdAt} />
-              <DataWithLabel label="Created By" text={createdItemUser} />
-              <DataWithLabel
-                label="Last Updated At"
-                text={resultItem.updatedAt}
-              />
-              <DataWithLabel label="Last Updated By" text={updatedItemUser} />
             </div>
           </div>
+          <div className="flex justify-end">
+            <Button
+              className="my-4"
+              onClick={() => {
+                navigate({ to: '/item-menu' })
+              }}
+            >
+              back to item menu
+            </Button>
+          </div>
         </div>
-        <div className="flex justify-end">
-          <Button
-            className="my-4"
-            onClick={() => {
-              navigate({ to: '/item-menu' })
-            }}
-          >
-            back to item menu
-          </Button>
-        </div>
-      </div>
+      )}
     </>
   )
 }
