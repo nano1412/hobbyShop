4c3fae1a873d96a2390bf3cac66305dc371ff6d7
nano1412_15irx11 <nano.khongsri@gmail.com>
2026-04-27 16:00:24 +0700

    feat header nav

--- frontend/src/components/common/Header.tsx
+++ frontend/src/components/common/Header.tsx
@@ -1,8 +1,10 @@
 import { authClient } from '@/lib/auth-client'
 import { Button } from '@mantine/core'
 import { notifications } from '@mantine/notifications'
+import { useNavigate } from '@tanstack/react-router'
 
 export default function Header() {
+  const navigate = useNavigate()
   const handleSignOut = async () => {
     try {
       await authClient.signOut()
@@ -21,7 +23,12 @@ export default function Header() {
   return (
     <header className="sticky top-0 z-50 border-b px-4 bg-blue-900 text-white flex justify-between">
       <nav className="page-wrap flex flex-wrap items-center gap-x-3 gap-y-2 py-3 sm:py-4 text-left">
-        <h1 className="text-2xl font-bold">Hobby Shop</h1>
+        <h1
+          className="text-2xl font-bold cursor-pointer"
+          onClick={() => navigate({ to: '/item-menu' })}
+        >
+          Hobby Shop
+        </h1>
       </nav>
       {!!session && (
         <div className="flex items-center text-right">
