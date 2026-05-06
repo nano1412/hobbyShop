1110ceb79ac11e42b8857827f68e5590cde2019f
nano1412_15irx11 <nano.khongsri@gmail.com>
2026-05-06 12:50:17 +0700

    Reapply "fix CSR"
    This reverts commit 7f93db4989570558ca08f59a2dc572946190694b.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import { devtools } from '@tanstack/devtools-vite'
-import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 
 import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
@@ -9,16 +8,7 @@ const config = defineConfig({
   resolve: {
     tsconfigPaths: true,
   },
-  plugins: [
-    devtools(),
-    tailwindcss(),
-    tanstackStart({
-      spa: {
-        enabled: true,
-      },
-    }),
-    viteReact(),
-  ],
+  plugins: [devtools(), tailwindcss(), viteReact()],
 })
 
 export default config
