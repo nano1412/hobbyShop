9304f0d6899cfddb3f3b7da2679f0ebd614cd685
nano1412_15irx11 <nano.khongsri@gmail.com>
2026-05-13 13:43:59 +0700

    fix prisma generate directory

--- backend/src/lib/db.ts
+++ backend/src/lib/db.ts
@@ -1,6 +1,6 @@
 // src/lib/db.ts
 import { PrismaPg } from '@prisma/adapter-pg'
-import { PrismaClient } from '../../generated/prisma/client.js'
+import { PrismaClient } from '@prisma/client'
 import { env } from '@/config/env'
 
 const adapter = new PrismaPg({
