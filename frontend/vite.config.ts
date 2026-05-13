f568096e7440e73bd0565431012a4e84c4f7c696
nano1412_15irx11 <nano.khongsri@gmail.com>
2026-05-14 06:39:16 +0700

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -17,7 +17,7 @@ const config = defineConfig({
         enabled: true,
       },
       prerender: {
-        enabled: process.env.RAILWAY_ENVIRONMENT ? false : true,
+        enabled: false,
       },
     }),
     viteReact(),
