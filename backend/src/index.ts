e04a59fbcab9dee7abe67a5c97026ee8b6ee21c4
nano1412_15irx11 <nano.khongsri@gmail.com>
2026-05-14 07:16:07 +0700

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,4 +2,8 @@ import 'dotenv/config'
 import { app } from './app'
 import { env } from './config/env'
 
-app.listen(env.PORT)
+app.listen({
+  port: env.PORT,
+  hostname: '0.0.0.0',
+})
+console.log(`Server listening on ${env.PORT}`)
